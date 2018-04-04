101adde442a75fced8b4a8137cec61e78c04f252
Unknown <jr0850050@otc.edu>
2018-04-04 11:48:29 -0500

    Save states are good for your health
    i think i spent 2 hours just getting this stuff to work even somewhat properly on my laptop

---
+++ resources/views/dice/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>With so many dice to choose from, youre sure to find something to suit your asthetics!</h1>
+@stop
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+    <a class="navbar-brand" href="#">My Game Archive</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"><span class="navbar-toggler-icon"></span></button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+             <li class="nav-item @if(Route::is('home')) active @endif"> 
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
+            </li>
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="#">Video Games</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Board Games</a>
+            </li> -->
+            <li class="nav-item @if(Route::is('dice.*')) active @endif">
+                <a href="{{ route('dice.index') }}" class="nav-link">Tabletop RPGs</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
