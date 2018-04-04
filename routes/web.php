96e49df22ef6c95a3c94e68b8084716f545ce3ff
Unknown <jr0850050@otc.edu>
2018-04-04 09:43:33 -0500

    ITS ALIVE!

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Dice', 'DiceController');
+Route::resource('Figurine', 'FigurineController');
+Route::resource('AdvBook', 'AdventureBookController');
